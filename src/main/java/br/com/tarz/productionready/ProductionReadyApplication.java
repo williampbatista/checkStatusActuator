a4be58072f44ab091461ca370640d04398d37037
William Batista <william.pessoa@vegasolutions.com.br>
2020-02-13 15:20:34 -0300

    criação do projeto

---
+++ src/main/java/br/com/tarz/productionready/ProductionReadyApplication.java
@@ -0,0 +1,13 @@
+package br.com.tarz.productionready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionReadyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tarz/productionready/ProductionReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tarz.productionready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
